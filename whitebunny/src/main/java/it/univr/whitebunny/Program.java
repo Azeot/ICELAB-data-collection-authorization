29e5bb469017999a06866185d0ffe33d1dd6be32
Azeot <azeot@Azeots-MBP.home-life.hub>
2022-02-08 14:13:12 +0100

    rpc client

--- whitebunny/src/main/java/it/univr/whitebunny/Program.java
+++ whitebunny/src/main/java/it/univr/whitebunny/Program.java
@@ -1,16 +1,22 @@
 package it.univr.whitebunny;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.stream.Stream;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Program {
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws IOException {
         final var ctx = new AnnotationConfigApplicationContext(Config.class);
         final var authClient = ctx.getBean(AuthClient.class);
         final var rabbitmqClient = ctx.getBean(RabbitmqClient.class);
+        final var file = Path.of("/Users/azeot/projects/payload");
+        final var payload = Files.readString(file);
         final var message = Stream.of(argv).findFirst().orElse("Hello world");
         final var oauthResponse = authClient.getOauthResponse();
-        rabbitmqClient.publish(oauthResponse.accessToken, message);
+        System.out.println(rabbitmqClient.publish(oauthResponse.accessToken, payload));
+        
     }
 }
--- whitebunny/src/main/java/it/univr/whitebunny/RabbitmqClient.java
+++ whitebunny/src/main/java/it/univr/whitebunny/RabbitmqClient.java
@@ -1,12 +1,16 @@
 package it.univr.whitebunny;
 
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.ConnectionFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
 
 public class RabbitmqClient {
     final static Logger logger = LoggerFactory.getLogger(RabbitmqClient.class);
@@ -17,16 +21,39 @@ public class RabbitmqClient {
     public RabbitmqClient(ConnectionFactory connectionFactory, String exchange) {
         this.connectionFactory = connectionFactory;
         this.exchange = exchange;
+
     }
 
-    public void publish(String token, String message) {
+    public String publish(String token, String message) {
         connectionFactory.setPassword(token);
         try (final var connection = connectionFactory.newConnection();
              final var channel = connection.createChannel()) {
-            channel.exchangeDeclare(exchange, "fanout");
-            channel.basicPublish(exchange, "", null, message.getBytes(StandardCharsets.UTF_8));
-            logger.info("Sent '{}'", message);
-        } catch (IOException | TimeoutException e) {
+                final String corrId = UUID.randomUUID().toString();
+                
+                final String msg = message.replaceAll("corr_id", "corrId");
+
+                String replyQueueName = channel.queueDeclare().getQueue();
+                AMQP.BasicProperties props = new AMQP.BasicProperties
+                        .Builder()
+                        .correlationId(corrId)
+                        .replyTo(replyQueueName)
+                        .build();
+        
+                channel.basicPublish("", exchange, props, msg.getBytes("UTF-8"));
+        
+                final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+        
+                String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+                    if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                        response.offer(new String(delivery.getBody(), "UTF-8"));
+                    }
+                }, consumerTag -> {
+                });
+        
+                String result = response.take();
+                channel.basicCancel(ctag);
+                return result;
+        } catch (InterruptedException | IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
     }
